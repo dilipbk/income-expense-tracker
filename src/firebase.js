a1058e6138368a120915da8f36d3c9166dd4c452
Dilip <dilipbk.dev@gmail.com>
2025-12-05 22:46:26 +0545

    feat: added firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -8,16 +8,15 @@ import {
   persistentLocalCache,
   persistentMultipleTabManager,
 } from "firebase/firestore";
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBw1aTPaix0hZ_CVmx16eNGbQoPIOOL1HE",
-  authDomain: "expense-tracker-1c50d.firebaseapp.com",
-  projectId: "expense-tracker-1c50d",
-  storageBucket: "expense-tracker-1c50d.appspot.com",
-  messagingSenderId: "181370316392",
-  appId: "1:181370316392:web:7101e2588fae1b912cfddd",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
